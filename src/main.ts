2af53881e4b6e8e28526c8657d79a4c7baf39b83
aftabhussain551 <aftabhussain@softmindsol.com>
2026-01-23 19:10:12 +0500

    update : cors configuration

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
